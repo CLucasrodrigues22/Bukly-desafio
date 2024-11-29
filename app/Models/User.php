2eb466ef738b1ad29b968a0e2c41e53c6e6c5b02
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-11-29 18:11:39 -0300

    feat: add relationships between User and Reservation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -60,5 +62,10 @@ class User extends Authenticatable
             'password' => 'hashed',
             'email_verified_at' => 'datetime',
         ];
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
     }
 }

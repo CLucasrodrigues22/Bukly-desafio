a6e315606581fecc77d4e034a4be0e839c0508f6
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-11-29 20:55:59 -0300

    feat: validate payload reservation edit

--- app/Http/Controllers/Reservation/ReservationController.php
+++ app/Http/Controllers/Reservation/ReservationController.php
@@ -4,18 +4,14 @@ namespace App\Http\Controllers\Reservation;
 
 use App\Http\Controllers\Controller;
 use App\Models\Reservation;
-use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\{Request, RedirectResponse};
 use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class ReservationController extends Controller
 {
-    /**
-     * Display a listing of the reservations for the authenticated user.
-     *
-     * @return Response
-     */
     public function index(): Response
     {
         $reservations = Reservation::where('user_id', auth()->id())->get();
@@ -25,21 +21,43 @@ class ReservationController extends Controller
         ]);
     }
 
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $validated = $request->validate([
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|unique:reservations',
             'check_in' => 'required|date_format:Y-m-d|after_or_equal:today',
             'check_out' => 'required|date_format:Y-m-d|after:check_in',
         ]);
 
-        // Gerar o slug antes de criar a reserva
+        // generate slug
         $slug = Str::slug($validated['name']);
 
-        // Criação da reserva após validação
+        // create
         $reservation = Reservation::create([...$validated, 'user_id' => auth()->id(), 'slug' => $slug]);
 
         return redirect()->route('reservations.index')->with('success', 'Reservation created successfully.');
+    }
+
+    public function update(Request $request, Reservation $reservation)
+    {
+        // Validação dos dados de entrada
+        $validated = $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('reservations')->where('user_id', auth()->id())->ignore($reservation->id), // Ignora a reserva atual
+            ],
+            'check_in' => 'required|date_format:Y-m-d|after_or_equal:today',
+            'check_out' => 'required|date_format:Y-m-d|after:check_in',
+        ]);
+
+        // Atualiza a reserva com os dados validados
+        $reservation->update($validated);
+
+        // Redireciona de volta com sucesso
+        return redirect()->route('reservations.index')
+            ->with('success', 'Reservation updated successfully.');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ Route::get('/', fn () => Inertia::render('Welcome'));
 Route::prefix('/reservations')->group(function () {
     Route::get('/', [Reservation\ReservationController::class, 'index'])->name('reservations.index');
     Route::post('/', [Reservation\ReservationController::class, 'store'])->name('reservations.store');
+    Route::put('/{reservation}', [Reservation\ReservationController::class, 'update'])->name('reservations.update');
     Route::get('/{reservation_share}/{token}', Reservation\ShareController::class);
 });

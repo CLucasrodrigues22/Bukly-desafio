4cf6cf22355c93623ae364c847a7a1e13b71fe0e
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-11-29 18:11:55 -0300

    feat: add reservation factory

---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => $this->faker->unique()->word(),
+            'slug' => $this->faker->unique()->slug(),
+            'check_in' => $this->faker->date(),
+            'check_out' => $this->faker->date(),
+        ];
+    }
+}

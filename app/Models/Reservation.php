13cff42cb24cdcbd411cd41784cd8facc00d046b
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-11-29 18:18:13 -0300

    feat: implementing filter Reservation for today in model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -20,5 +22,16 @@ class Reservation extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Scope a query to only include reservations with 'check_in' equal to today.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeCheckInToday(Builder $query): Builder
+    {
+        return $query->whereDate('check_in', Carbon::today());
     }
 }

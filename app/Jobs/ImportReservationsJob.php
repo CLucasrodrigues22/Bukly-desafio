b69ec68f57fc7f1430ef725c26dfc4c84fa00bf2
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-11-29 23:24:40 -0300

    feat: Import reservations from the API

--- app/Jobs/ImportReservationsJob.php
+++ app/Jobs/ImportReservationsJob.php
@@ -2,8 +2,11 @@
 
 namespace App\Jobs;
 
+use App\Models\Reservation;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class ImportReservationsJob implements ShouldQueue
 {
@@ -20,8 +23,25 @@ class ImportReservationsJob implements ShouldQueue
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle()
     {
-        //
+        // Chama a API externa para pegar as reservas
+        $response = Http::get('https://api.example.com/reservations');
+
+        // Verifica se a resposta foi bem-sucedida
+        if ($response->successful()) {
+            $reservations = $response->json();
+
+            foreach ($reservations as $reservationData) {
+                // Atribui os dados e cria a reserva
+                Reservation::create([
+                    'user_id' => $reservationData['user_id'],
+                    'name' => $reservationData['name'],
+                    'slug' => Str::slug($reservationData['name']),
+                    'check_in' => $reservationData['check_in'],
+                    'check_out' => $reservationData['check_out'],
+                ]);
+            }
+        }
     }
 }
